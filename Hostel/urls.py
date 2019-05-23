2ae0d94bed60ae593d4dd927346e2c8dbb92ced8
abin <abinrajkb@gmail.com>
2019-05-23 17:32:10 +0530

    sa

--- Hostel/urls.py
+++ Hostel/urls.py
@@ -15,9 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
-from django.conf import settings
-from django.conf.urls.static import static
+import re
+from urllib.parse import urlsplit
 
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.urls import re_path
+from django.views.static import serve
+
+
+def static(prefix, view=serve, **kwargs):
+    """
+    Return a URL pattern for serving files in debug mode.
+
+    from django.conf import settings
+    from django.conf.urls.static import static
+
+    urlpatterns = [
+        # ... the rest of your URLconf goes here ...
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    """
+    if not prefix:
+        raise ImproperlyConfigured("Empty static prefix not permitted")
+    return [
+        re_path(r'^%s(?P<path>.*)$' % re.escape(prefix.lstrip('/')), view, kwargs=kwargs),
+    ]
 
 
 urlpatterns = [
@@ -30,4 +52,3 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-print(urlpatterns)
