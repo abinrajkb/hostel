c6a0f3f299acf41a3cd7f9e481a8e3082c260f5f
abin <abinrajkb@gmail.com>
2019-05-28 00:45:21 +0530

    Included the instructions page.

--- Homepage/views.py
+++ Homepage/views.py
@@ -5,5 +5,10 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'Homepage/homepage.html')
 
+
 def details(request):
     return render(request, 'Homepage/details.html')
+
+
+def instructions(request):
+    return render(request, 'Homepage/instructions.html')
